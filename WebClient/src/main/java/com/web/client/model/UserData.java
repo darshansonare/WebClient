fe83aacea251e51889bec8969a55a7f02dcf4b5b
Darshan Sonare <104987000+darshansonare@users.noreply.github.com>
2025-11-25 15:40:37 +0530

    Add files via upload

---
+++ WebClient/src/main/java/com/web/client/model/UserData.java
@@ -0,0 +1,12 @@
+package com.web.client.model;
+
+import lombok.Data;
+
+@Data
+public class UserData {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+}
